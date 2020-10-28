3bd53fd6f1e70fb0fb7677a7327c7fc458fe0527
zoguxy <bsaa.stars7661@gmail.com>
2020-10-29 00:17:56 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :name1,  presence: true, length: { maximum: 126 }
+  validates :name2,  presence: true, length: { maximum: 126 }, format: { with: /\A[ぁ-んァ-ヶー－]+\z/ }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, 
+  uniqueness: { case_sensitive: false }
+  validates :address, length: { maximum: 126 }
+end
