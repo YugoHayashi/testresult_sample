3e1f4c73784cfb92072d83eb2eb273608da2c130
zoguxy <bsaa.stars7661@gmail.com>
2020-10-29 11:05:45 +0900

    create user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -6,7 +15,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      flash[:success] = "登録が完了しました。"
+      redirect_to root_path
     else
       render 'new'
     end
