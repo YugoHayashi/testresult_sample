241496502b27039a6046f071722c875ff0173b4a
zoguxy <bsaa.stars7661@gmail.com>
2020-10-29 16:44:05 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -17,15 +18,40 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "登録が完了しました。"
-      redirect_to root_path
+      redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "削除完了"
+    redirect_to users_url
   end
 
   private
 
     def user_params
       params.require(:user).permit(:name1, :name2, :email, :address)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
